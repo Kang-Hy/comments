01ff16109d6985ea8e7d809f472ef38b19477c33
Kang-Hy <130884670+Kang-Hy@users.noreply.github.com>
2023-11-29 20:18:01 +0800

    Update FollowController.java
    1 change to 12

--- src/main/java/com/khy/controller/FollowController.java
+++ src/main/java/com/khy/controller/FollowController.java
@@ -25,7 +25,7 @@ public class FollowController {
 
     @PutMapping("/{id}/{isFollow}")
     public Result follow(@PathVariable("id") Long followUserId, @PathVariable("isFollow") Boolean isFollow) {
-        log.info("1");
+        log.info("12");
         return followService.follow(followUserId, isFollow);
     }
 
