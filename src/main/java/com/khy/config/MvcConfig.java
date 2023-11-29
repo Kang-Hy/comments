8f58e4f80fe1d48e061e913fe1db538679de4b67
Kang-Hy <1906393947@qq.com>
2023-11-29 22:05:05 +0800

    1

--- src/main/java/com/khy/config/MvcConfig.java
+++ src/main/java/com/khy/config/MvcConfig.java
@@ -2,13 +2,14 @@ package com.khy.config;
 
 import com.khy.utils.LoginInterceptor;
 import com.khy.utils.RefreshTokenInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Resource;
-
+@Slf4j
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
@@ -17,6 +18,7 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        log.info("1");
         // 登录拦截器
         registry.addInterceptor(new LoginInterceptor())
                 .excludePathPatterns(
--- src/main/java/com/khy/config/MybatisConfig.java
+++ src/main/java/com/khy/config/MybatisConfig.java
@@ -3,13 +3,15 @@ package com.khy.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+@Slf4j
 @Configuration
 public class MybatisConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        log.info("1");
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
