3245fc243631d3632630efa018d9a99ed5c6d158
Kang-Hy <1906393947@qq.com>
2023-12-01 23:10:01 +0800

    ok

--- src/main/java/com/khy/Application.java
+++ src/main/java/com/khy/Application.java
@@ -4,10 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.stream.Stream;
+
 @MapperScan("com.khy.mapper")
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
     }
 }
